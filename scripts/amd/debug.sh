e84cd3fc61e09aaedce7a4e7f6e0ab62e059e6c9
Michael Melesse <micmelesse@gmail.com>
2021-12-14 12:49:09 -0600

    add scripts

--- scripts/amd/debug.sh
+++ scripts/amd/debug.sh
@@ -1,6 +1,7 @@
+sudo apt install gdb -y
 gdb -ex "set pagination off" \
     -ex "file python" \
-    -ex 'run -m pytest --capture=tee-sys --verbose "python/test/unit/language/test_core.py::test_load_and_store_op[float64]"' \
+    -ex 'run -m pytest --capture=tee-sys --verbose "tests/unit/test_sparse_attention.py::test_softmax[dtype1-256-16]"' \
     -ex "bt" \
     -ex "set confirm off" \
     -ex "q" \
--- scripts/amd/run.sh
+++ scripts/amd/run.sh
@@ -1,3 +1,7 @@
+clean
+bash scripts/amd/clean.sh
 bash scripts/amd/install_triton.sh
 bash scripts/amd/build.sh
 bash scripts/amd/test.sh
+# bash scripts/amd/debug.sh
+bash scripts/amd/post.sh
