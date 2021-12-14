4211e58416cde65d692b27ddb6f7723452a132aa
Michael Melesse <micmelesse@gmail.com>
2021-12-14 12:30:43 -0600

    update scripts

--- scripts/amd/run.sh
+++ scripts/amd/run.sh
@@ -1,3 +1,3 @@
-sh scripts/amd/install_triton.sh
-sh scripts/amd/build.sh
-sh scripts/amd/test.sh
+bash scripts/amd/install_triton.sh
+bash scripts/amd/build.sh
+bash scripts/amd/test.sh
--- scripts/amd/test.sh
+++ scripts/amd/test.sh
@@ -1,2 +1,3 @@
-pytest --verbose tests/unit/test_sparse_attention.py
+pytest --verbose tests/unit/test_sparse_attention.py |& tee /dockerx/DeepSpeed_rocm/test_sparse_attention.log
+# pytest --verbose tests/unit/test_sparse_attention.py::test_softmax[dtype1-256-16]
 # pytest --verbose tests/unit/test_sparse_attention.py::test_softmax 
